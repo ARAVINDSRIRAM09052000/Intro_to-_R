5dabaee0f59442617884ec6873455ee7803dd9f0
Aravind Sriram <aaravindsriram@gmail.com>
2025-02-04 10:19:05 +0530

    new commit

--- intro.R
+++ intro.R
@@ -8,4 +8,45 @@ getwd()
 a=3
 b=9
 a+b
-log9
+log(9)
+log(4^2)
+log(100,10)
+x=7
+y=5
+x!=y
+5==9
+F*50
+T*50
+5>2
+3==3 & 4>5
+3==3 | 4>5
+T&T
+T&F
+F&F
+T|T
+T|F
+
+T&F|F
+
+
+v1=5:100
+v1
+head(v1)
+tail(v1)
+v2=4.8:9.6
+v2
+v3=seq(4,9, by =0.4)
+v3
+v4=seq(10,20, length=5)
+v4
+v5=seq(10,90, by =5)
+v5
+v5[-2]
+v6= c(1,2,3,4)
+v7= c(5,6)
+v6+v7
+v7+v6
+v8 =c(10,17,25,4,6,9,14)
+sort(v8)
+sort(v8,decreasing=T)
+g
